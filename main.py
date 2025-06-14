05ef6102e125ba61cf61c84a740fdd1bb4229376
Noureddine Maazouz <nmaazouz@student.1337.ma>
2025-06-14 15:26:07 +0000

    Remove command parser and related tests to streamline command handling in the task management system.

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
-from command_parser import parse_command
-from icecream import ic
 from TaskManager import TaskManager
-
-def execute(taskManager: TaskManager, command: str, argument: str):
-    command = command.lower()
-    
-    match command:
-        case "add":
-            print(f"Adding task: {argument}")
-            taskManager.add_task(argument)
-        case "remove":
-            print(f"Removing task: {argument}")
-        case "list":
-            taskManager.list_tasks()
-        case "exit":
-            print("Exiting....")
-            exit(0)
-        case _:
-            print(f"Unknown command: {command}")
-
 from CLI import CLI
 from storage import Storage
 
